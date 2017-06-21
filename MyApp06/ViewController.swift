6c2ae1afcf55e5182b7c3d0f3ad2a8059e1a58a1
iii <iii@userde-MacBook-6.local>
2017-06-21 12:04:33 +0800

    v1

--- MyApp06/ViewController.swift
+++ MyApp06/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var btnOK:UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        btnOK = UIButton(type: UIButtonType.system)
+        btnOK?.frame = CGRect(x: 0, y: 20, width: 40, height: 40)
+        btnOK?.setTitle("OK", for: UIControlState.normal)
+        view.addSubview(btnOK!)
+        
+        switch traitCollection.userInterfaceIdiom {
+        case .pad:
+            print("pad")
+        case .phone:
+            print("phone")
+                default:
+            break
+        }
     }
-
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        switch UIDevice.current.orientation {
+        case .faceUp:
+            print("faceup")
+        case.faceDown:
+            print("facedown")
+        case .landscapeLeft:
+            print("Home -> ")
+        case .landscapeRight:
+            print("<- Home ")
+        case .portrait:
+            print("normal")
+        case.portraitUpsideDown:
+            print("normal 顛倒")
+        default:
+            break
+        }
+        print("size => w = \(size.width), h = \(size.height)")
+    }
+ 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
